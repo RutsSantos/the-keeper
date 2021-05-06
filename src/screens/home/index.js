6f6ea16f59a1677cb5e0999f78e0e434f949afc3
Rut Santos <rsantos@intellisys.com.do>
2021-05-06 09:30:42 -0400

    Feature-1 created the app stack navigator

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,2 +1,11 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
+
+
+export default function Home({ navigation }) {
+    return (
+        <View style={{backgroundColor: 'red', justifyContent: "center", flex: 1}}>
+            <Text>Home</Text>
+        </View>
+    )
+}
--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -1,2 +1,10 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
+
+export default function Profile({ navigation }) {
+    return (
+        <View>
+            <Text>Profile</Text>
+        </View>
+    )
+}
